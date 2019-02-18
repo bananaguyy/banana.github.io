f64077d2323b6a8d94e153385930db458fc91e6c
bananaguyy <bananaguyyyy@gmail.com>
2019-02-19 05:17:31 +0600

    v2

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>banana</title>
+  <link rel="stylesheet" href="bootstrap-4.1.3-dist\css\bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="jumbotron jumbotron-fluid">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-2 my-auto">
+          <span><img src="img/banana.png" width="100%" alt=""><span>
+        </div>
+        <div class="col-8">
+          <span class="display-2 logo">banana !</span>
+          <h2 class="display-1 galaxy">Welcome</h1>
+          <span class="lead">Manage and track your Class...</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container text-center galaxy1">
+
+    <form class="" action="add_record.php" method="POST">
+      <span id="showdate" class="display-3 colorr"></span>
+      <div class="py-5">
+            <button type="submit" class="btn btn-outline-success btn-lg">Attend</button>
+        <button type="button" class="btn btn-primary btn-lg my-5">Review</button>
+      </div>
+
+    </form>
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  ,<script src="main.js" charset="utf-8"></script>
+
+  <script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,5 @@
+
+var date= new Date();
+var show=date.toDateString();
+
+document.getElementById('showdate').innerHTML=show;
